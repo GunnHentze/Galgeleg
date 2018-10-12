9b60e7d7396dc694f625524dc27f380e11340aaf
Gunn Hentze <s145494@student.dtu.dk>
2018-10-12 19:20:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gunnh/galgeleg/GalgelegAkt.java
@@ -0,0 +1,13 @@
+package com.example.gunnh.galgeleg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalgelegAkt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galgeleg_akt);
+    }
+}
